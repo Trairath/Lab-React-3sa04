1e5d34a5064f6a7f1255462263890dd6be3550a6
Trairath <6210110712@psu.ac.th>
2023-08-14 21:53:11 +0700

    Connect api weather

--- components/Weather.js
+++ components/Weather.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 import { ImageBackground, Text , StyleSheet} from 'react-native';
 import Forecast from './Forecast';
 
@@ -8,6 +8,24 @@ export default function Weather(props) {
         description: '-',
         temp: 0
         }) 
+    
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=2f3db196652616fac189c3c87a00d6d1`)
+                .then((response) => response.json())
+                .then((json) => {
+                    setForecastInfo({
+                        main: json.weather[0].main,
+                        description: json.weather[0].description,
+                        temp: json.main.temp
+                    });
+                })
+                .catch((error) => {
+                    console.warn(error);
+                });
+            }
+        }, [props.zipCode])
 
     return (
         <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
