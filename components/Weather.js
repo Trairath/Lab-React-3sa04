199e1faabf20f086a1d97d994152ca209877be33
Trairath <6210110712@psu.ac.th>
2023-08-14 18:06:02 +0700

    Flex Box

--- components/Weather.js
+++ components/Weather.js
@@ -1,9 +1,30 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { ImageBackground, Text , StyleSheet} from 'react-native';
+import Forecast from './Forecast';
 
 export default function Weather(props) {
+    const [forecastInfo, setForecastInfo] = useState({
+        main: '-',
+        description: '-',
+        temp: 0
+        }) 
+
     return (
-    <Text>{props.zipCode}</Text>
+        <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
+            <Text>zipCode</Text>
+            <Text>{props.zipCode}</Text>
+            <Forecast {...forecastInfo} />
+        </ImageBackground>
+        
     );
    }
-   
+
+const styles = StyleSheet.create({
+    backdrop: {
+        flexDirection:'column',
+        justifyContent:'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%'
+    }
+})
